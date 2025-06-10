7d727b348641ad28d6414da2f12a1ac18697eaa3
dmittruc <dimon22012201@gmail.com>
2025-06-10 13:53:55 +0300

    create and delete tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,50 @@
-import { TypedUseSelectorHook, useSelector } from 'react-redux';
+import { TypedUseSelectorHook, useSelector, useDispatch } from 'react-redux';
 import './App.css';
 import TaskList from './components/TaskList';
 import { RootState } from './store';
+import { useState } from 'react';
+import { addTaskAction, removeTaskAction } from './store/actions';
+import { v4 } from 'uuid';
+
+
 
 const App = () => {
   const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+  const dispatch = useDispatch();
   const tasks = useAppSelector((state: any) => state.tasks);
 
-  // const [title, setTitle] = useState<string>('');
-  // const [description, setDescription] = useState<string>('');
+  const [title, setTitle] = useState<string>('');
+  const [description, setDescription] = useState<string>('');
 
-  // const createNewTask = (title, description) => {}
+const createNewTask = (title: string, description: string) => {
+  if (!title.trim()) return;
+
+  const newTask = {
+    id: v4(),
+    title,
+    description,
+    completed: false,
+  };
+
+  dispatch(addTaskAction({ task: newTask }));
+
+  setTitle('');
+  setDescription('');
+};
 
   return (
     <div className="App">
-      {/* <button onClick={() => createNewTask(title, description)}>Create Task</button> */}
+      <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Title"
+      />
+      <input
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        placeholder="Description"
+      />
+      <button onClick={() => createNewTask(title, description)}>Create Task</button>
       <TaskList tasks={tasks} />
     </div>
   );
--- src/components/TaskListItem/index.tsx
+++ src/components/TaskListItem/index.tsx
@@ -1,14 +1,21 @@
+import { useDispatch } from "react-redux";
 import { ITask } from "../../interfaces"
+import { removeTaskAction } from "../../store/actions";
 
 interface IProps {
     task: ITask
 }
 
 const TaskListItem = ({task}: IProps) => {
+const dispatch = useDispatch();
+    const deleteTask = (id: string) => {
+        dispatch(removeTaskAction({task}));
+    }
     return (
         <div key={task.id}>
             <h3>{task.title}</h3>
             <p>{task.description}</p>
+            <button onClick={() => deleteTask(task.id)}>Delete</button>
         </div>
     )
 }
